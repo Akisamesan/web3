10e347cc1267e3b7283a6b0f1bd1f3de78af8258
Akisamesan <rainyfall47@gmail.com>
2024-04-10 13:09:43 +0900

    Initial commit

---
+++ 1.py
@@ -0,0 +1,8 @@
+class mathclass:
+    def mathdef():
+        for i in range(12):
+            for j in range(12):
+                print(i*j, end=" ")
+            print()
+math = mathclass
+math.mathdef()
---
+++ 2.js
@@ -0,0 +1,6 @@
+let str = '';
+
+for (let i = 0; i < 9; i++) {
+    str += i;
+    document.body.innerHTML = str;
+}
